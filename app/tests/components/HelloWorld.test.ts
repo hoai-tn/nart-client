9da6701a1aa62d0be23404b9ce5c19ed8f30fd67
Hoai Tran <tranngochoai02051999@gmail.com>
2024-12-26 09:25:25 +0700

    feat: add Vitest configuration and unit tests for HelloWorld component

---
+++ app/tests/components/HelloWorld.test.ts
@@ -0,0 +1,18 @@
+import { mount } from '@vue/test-utils'
+import { test, expect } from 'vitest'
+import HelloWord from '~/components/HelloWord.vue'
+// The component to test
+
+test('displays message', () => {
+  const wrapper = mount(HelloWord, {
+    props: {
+      name: 'Hello world',
+    },
+  })
+  console.log(wrapper.get('h1').text())
+
+  expect(wrapper.text()).toBeTruthy()
+
+  // Assert the rendered text of the component
+  expect(wrapper.text()).toContain('Hello world')
+})
